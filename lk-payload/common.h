f8566ad2dd47b1a47860948d1e3627fa57a4685a
Joel Stein <js@funkturm.net>
2019-03-13 06:36:28 +0100

    update lk-payload for suez

--- lk-payload/common.h
+++ lk-payload/common.h
@@ -9,14 +9,15 @@ struct device_t {
     size_t (*write)(struct device_t *dev, void *src, uint64_t block_off, size_t size, uint32_t part);
 };
 
-struct device_t* (*get_device)() = (void*)0x4BD2B2F1;
-void (*cache_clean)(void *addr, size_t sz) = (void*)0x4BD31444;
+struct device_t* (*get_device)() = (void*)0x4BD14699;
+void (*cache_clean)(void *addr, size_t sz) = (void*)0x4BD2A6DC;
+size_t (*video_printf)(const char *format, ...) = (void *)0x4BD30A1F;
 
-uint32_t* g_boot_mode = (uint32_t*) 0x4BD5C2AC;
+uint32_t* g_boot_mode = (uint32_t*) 0x4BD69354;
 
 #define PAYLOAD_DST 0x41000000
-#define PAYLOAD_SRC 0x200000
-#define PAYLOAD_SIZE 0x200000
+#define PAYLOAD_SRC 0x80000
+#define PAYLOAD_SIZE 0x80000
 
 #define BOOT0_PART 1
 #define USER_PART 8
--- lk-payload/main.c
+++ lk-payload/main.c
@@ -23,45 +23,53 @@ void _putchar(char character)
     low_uart_put(character);
 }
 
-int (*original_read)(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) = (void*)0x4BD2AE2D;
-int (*app)() = (void*)0x4BD341D5;
+int (*original_read)(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) = (void*)0x4BD14271;
+int (*app)() = (void*)0x4BD2CBC1;
 
-uint64_t g_boot_a, g_boot_aa, g_boot_b, g_boot_bb, g_lk_a, g_lk_b, g_misc, g_recovery;
-uint8_t boot_recovery = 0;
+uint64_t g_boot, g_boot_x, g_lk, g_misc, g_recovery, g_recovery_x;
+
+void hex_dump(const void* data, size_t size) {
+    char ascii[17];
+    size_t i, j;
+    ascii[16] = '\0';
+    for (i = 0; i < size; ++i) {
+        printf("%02X ", ((unsigned char*)data)[i]);
+        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+            ascii[i % 16] = ((unsigned char*)data)[i];
+        } else {
+            ascii[i % 16] = '.';
+        }
+        if ((i+1) % 8 == 0 || i+1 == size) {
+            printf(" ");
+            if ((i+1) % 16 == 0) {
+                printf("\n");
+                // printf("|  %s \n", ascii);
+            } else if (i+1 == size) {
+                ascii[(i+1) % 16] = '\0';
+                if ((i+1) % 16 <= 8) {
+                    printf(" ");
+                }
+                for (j = (i+1) % 16; j < 16; ++j) {
+                    printf("   ");
+                }
+                // printf("|  %s \n", ascii);
+                printf("\n");
+            }
+        }
+    }
+}
+
 
 int read_func(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) {
     printf("read_func hook\n");
-    //hex_dump((void *)0x4BD00000, 0x100);
-    //printf("block_off 0x%08X 0x%08X\n", block_off, *(&(block_off)+4));
+    //hex_dump((void *)0x4BD003DC, 0x100);
+    printf("block_off 0x%08X 0x%08X\n", block_off, *(&(block_off)+4));
+    printf("dev 0x%08X dst 0x%08X sz 0x%08X part 0x%08X\n", dev, dst, sz, part);
     int ret = 0;
-    if(block_off == g_boot_a * 0x200) {
-      if(boot_recovery) {
-        block_off = g_recovery * 0x200;
-      }
-      else {
-        block_off = g_boot_aa * 0x200;
-      }
-    } else if(block_off == (g_boot_a * 0x200) + 0x800) {
-      if(boot_recovery) {
-         block_off = (g_recovery * 0x200) + 0x800;
-      }
-      else {
-        block_off = (g_boot_aa * 0x200) + 0x800;
-      }
-    } else if(block_off == g_boot_b * 0x200) {
-      if(boot_recovery) {
-        block_off = g_recovery * 0x200;
-      }
-      else {
-        block_off = g_boot_bb * 0x200;
-      }
-    } else if(block_off == (g_boot_b * 0x200) + 0x800) {
-      if(boot_recovery) {
-        block_off = (g_recovery * 0x200) + 0x800;
-      }
-      else {
-        block_off = (g_boot_bb * 0x200) + 0x800;
-      }
+    if(block_off == g_boot * 0x200) {
+      block_off = g_boot_x * 0x200;
+    } else if(block_off == g_recovery * 0x200) {
+      block_off = g_recovery_x * 0x200;
     }
     return original_read(dev, block_off, dst, sz, part);
 }
@@ -75,30 +83,24 @@ static void parse_gpt() {
         uint8_t *name = ptr + 0x38;
         uint32_t start;
         memcpy(&start, ptr + 0x20, 4);
-        if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00\x61\x00\x00\x00", 14) == 0) {
-            printf("found boot_a at 0x%08X\n", start);
-            g_boot_a = start;
-        } else if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00\x61\x00\x61\x00\x00\x00", 16) == 0) {
-            printf("found boot_aa at 0x%08X\n", start);
-            g_boot_aa = start;
-        } else if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00\x62\x00\x00\x00", 14) == 0) {
-            printf("found boot_b at 0x%08X\n", start);
-            g_boot_b = start;
-        } else if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00\x62\x00\x62\x00\x00\x00", 16) == 0) {
-            printf("found boot_bb at 0x%08X\n", start);
-            g_boot_bb = start;
-        } else if (memcmp(name, "l\x00k\x00_\x00\x61\x00\x00\x00", 10) == 0) {
-            printf("found lk_a at 0x%08X\n", start);
-            g_lk_a = start;
-        } else if (memcmp(name, "l\x00k\x00_\x00\x62\x00\x00\x00", 10) == 0) {
-            printf("found lk_b at 0x%08X\n", start);
-            g_lk_b = start;
-        } else if (memcmp(name, "m\x00i\x00s\x00\x63\x00\x00\x00", 10) == 0) {
+        if (memcmp(name, "b\x00o\x00o\x00t\x00\x00\x00", 10) == 0) {
+            printf("found boot at 0x%08X\n", start);
+            g_boot = start;
+        } else if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00x\x00\x00\x00", 14) == 0) {
+            printf("found boot_x at 0x%08X\n", start);
+            g_boot_x = start;
+        } else if (memcmp(name, "l\x00k\x00\x00\x00", 6) == 0) {
+            printf("found lk at 0x%08X\n", start);
+            g_lk = start;
+        } else if (memcmp(name, "M\x00I\x00S\x00\x43\x00\x00\x00", 10) == 0) {
             printf("found misc at 0x%08X\n", start);
             g_misc = start;
         } else if (memcmp(name, "r\x00\x65\x00\x63\x00o\x00v\x00\x65\x00r\x00y\x00\x00\x00", 18) == 0) {
             printf("found recovery at 0x%08X\n", start);
             g_recovery = start;
+        } else if (memcmp(name, "r\x00\x65\x00\x63\x00o\x00v\x00\x65\x00r\x00y\x00_\x00x\x00\x00\x00", 22) == 0) {
+            printf("found recovery_x at 0x%08X\n", start);
+            g_recovery_x = start;
         }
     }
 }
@@ -106,27 +108,25 @@ static void parse_gpt() {
 int main() {
     int ret = 0;
     printf("This is LK-payload by xyz. Copyright 2019\n");
-    printf("64-Bit version for biscuit by k4y0z. Copyright 2019\n");
+    printf("64-Bit version for suez by k4y0z. Copyright 2019\n");
 
     int fastboot = 0;
 
     parse_gpt();
 
-    if (!g_boot_aa || !g_boot_bb || !g_lk_a) {
+    if (!g_boot_x || !g_recovery_x || !g_lk) {
         printf("failed to find boot, recovery or lk\n");
         printf("falling back to fastboot mode\n");
         fastboot = 1;
     }
 
     unsigned char overwritten[] = {
-        0x6C, 0xBC, 0x05, 0x00, 0x60, 0xBC, 0x05, 0x00, 0x2D, 0xE9, 0xF8, 0x43, 0x5D, 0x48, 0x5E, 0x4D,
-        0x78, 0x44, 0x5E, 0x4F, 0x39, 0xF0, 0x0E, 0xF8, 0x7D, 0x44, 0x29, 0x68, 0x7F, 0x44, 0x69, 0xBB,
-        0x5B, 0x4C, 0x4F, 0xF4, 0x70, 0x52, 0x7C, 0x44, 0x20, 0x46, 0x3A, 0xF0, 0x0C, 0xE8, 0x20, 0x46,
-        0x30, 0xF0, 0x00, 0xFE, 0x00, 0x28, 0x40, 0xF0, 0x9D, 0x80, 0x20, 0x46, 0x2C, 0x60, 0xFF, 0xF7,
+        0x50, 0x7c, 0x06, 0x00, 0x44, 0x7c, 0x06, 0x00
     };
+    memcpy((void*)0x4BD003DC, overwritten, sizeof(overwritten));
 
-    memcpy((void*)0x4BD003C0, overwritten, sizeof(overwritten));
-    //hex_dump((void*)0x4BD003C0, 0x100);
+    uint32_t **argptr = (void*)0x4BD00020;
+    *argptr = (void*)0x4be7f288;
 
     uint8_t bootloader_msg[0x10] = { 0 };
     void *lk_dst = (void*)0x4BD00000;
@@ -139,48 +139,32 @@ int main() {
       // Read amonet-flag from MISC partition
       dev->read(dev, g_misc * 0x200, bootloader_msg, 0x10, USER_PART);
       //dev->read(dev, g_misc * 0x200 + 0x4000, bootloader_msg, 0x10, USER_PART);
-      //printf("bootloader_msg: %s\n", bootloader_msg);
-    }
+      printf("bootloader_msg: %s\n", bootloader_msg);
 
-    uint8_t *tmp = (void*)0x45000020;
-
-/*
-    uint32_t* boot_reason = (uint32_t*)((*(uint32_t*)0x4BD664E0) + 256);
-    char* bootreason = (char*)(16 * *boot_reason + 0x4BD55FFC);
-    uint32_t* usb_config = (uint32_t*)((*(uint32_t*)0x4BD664E0) + 264);
-
-    printf("boot_reason %u\n", *boot_reason);
-    printf("bootreason %s\n", bootreason);
-    for(int i = 0; i < 12; ++i) {
-      printf("bootreason (%u) %s\n", i, (char*)(16 * i + 0x4BD55FFC));
-    }
-*/
-
-    if (strncmp(tmp, "FASTBOOT_PLEASE", 15) == 0) {
+      // temp flag on MISC
+      if(strncmp(bootloader_msg, "boot-amonet", 11) == 0) {
         fastboot = 1;
-    }
+        // reset flag
+        memset(bootloader_msg, 0, 0x10);
+        dev->write(dev, bootloader_msg, g_misc * 0x200, 0x10, USER_PART);
+      }
 
-    // flag on MISC
-    else if(strncmp(bootloader_msg, "boot-amonet", 11) == 0) {
-      fastboot = 1;
-      // reset flag
-      memset(bootloader_msg, 0, 11);
-      dev->write(dev, bootloader_msg, g_misc * 0x200, 11, USER_PART);
+      // perm flag on MISC
+      else if(strncmp(bootloader_msg, "FASTBOOT_PLEASE", 15) == 0) {
+        // only reset flag in recovery-boot
+        if(*g_boot_mode == 2) {
+          memset(bootloader_msg, 0, 0x10);
+          dev->write(dev, bootloader_msg, g_misc * 0x200, 0x10, USER_PART);
+        }
+        else {
+          fastboot = 1;
+        }
+      }
     }
 
     // factory and factory advanced boot
     else if(*g_boot_mode == 4 || *g_boot_mode == 6){
         fastboot = 1;
-    }
-
-    // Use seperate recovery partition
-    else if(*g_boot_mode == 2){
-        //fastboot = 1;
-        if(g_recovery) {
-          boot_recovery = 1;
-          //If we don't set boot_mode to 0, USB is disabled.
-          *g_boot_mode = 0;
-        }
     }
 
 #ifdef RELOAD_LK
@@ -194,36 +178,33 @@ int main() {
     if (fastboot) {
         printf("well since you're asking so nicely...\n");
 
-        patch = (void*)0x4BD34330;
-        *patch = 0xE795;
+        patch = (void*)0x4BD2CC00;
+        *patch = 0xE006;
+
+        video_printf("=> HACKED FASTBOOT mode: (%d) - xyz, k4y0z\n", *g_boot_mode);
+    }
+    else if(*g_boot_mode == 2) {
+      video_printf("=> RECOVERY mode...");
     }
 
     printf("g_boot_mode %u\n", *g_boot_mode);
+
     // device is unlocked
-    patch = (void*)0x4BD1D2FC;
+    patch = (void*)0x4BD042BC;
     *patch++ = 0x2001; // movs r0, #1
     *patch = 0x4770;   // bx lr
 
-    // This enables adb-root-shell
     // amzn_verify_limited_unlock (to set androidboot.unlocked_kernel=true)
-    patch = (void*)0x4BD1D51C;
+    patch = (void*)0x4BD04484;
     *patch++ = 0x2000; // movs r0, #0
     *patch = 0x4770;   // bx lr
-
-    /*
-    // is_prod_device
-    patch = (void*)0x4BD1DB0A;
-    *patch = 0x2000; // movs r0, #0
-    */
 
     //printf("(void*)dev->read 0x%08X\n", (void*)dev->read);
     //printf("(void*)&dev->read 0x%08X\n", (void*)&dev->read);
 
     // Force uart enable
-    char* disable_uart = (char*)0x4BD4B0F8;
+    char* disable_uart = (char*)0x4BD59701;
     strcpy(disable_uart, "printk.disable_uart=0");
-    disable_uart = (char*)0x4BD4A56C;
-    strcpy(disable_uart, " printk.disable_uart=0");
 
     uint32_t *patch32;
 
@@ -231,26 +212,26 @@ int main() {
 
     original_read = (void*)dev->read;
 
-    patch32 = (void*)0x4BD57670;
+    patch32 = (void*)0x4BD630E4;
     *patch32 = (uint32_t)read_func;
 
     patch32 = (void*)&dev->read;
     *patch32 = (uint32_t)read_func;
 
-    patch32 = (void*)0x4BD641F4;
-    *patch32 = 1; // // force 64-bit linux kernel
-
     // patch max-download-size to accommodate for payload
-    patch32 = (void*)0x4BD34CE0;
-    *patch32 = 0x0380F503; // ADD.W	R3, R3, #0x400000
+    patch32 = (void*)0x4BD2D55C;
+    *patch32 = 0x0380F50E; // ADD.W	R3, LR, #0x400000
+
+    // Force 64-bit kernel
+    patch32 = (void*)0x4BD701A0;
+    *patch32 = 1;
 
     printf("Clean lk\n");
     cache_clean(lk_dst, LK_SIZE);
 
 #ifdef RELOAD_LK
     printf("About to jump to LK\n");
-
-    uint32_t **argptr = (void*)0x4BD00020;
+    
     uint32_t *arg = *argptr;
     arg[0x53] = 4; // force 64-bit linux kernel
 
