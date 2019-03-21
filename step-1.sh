9537e3612aba6c2f980e1a8bcfc693cde5027a7d
Joel Stein <js@funkturm.net>
2019-03-21 19:20:14 +0100

    More checks and add brick option

--- step-1.sh
+++ step-1.sh
@@ -6,7 +6,55 @@ set -e
 
 adb wait-for-device
 
+max_tee=263
+max_lk=2
+max_pl=5
+
+product=$(adb shell getprop ro.product.name | dos2unix)
+
+if [ "$product" != "suez" ] ; then
+  echo "This is only for the \"suez\" (Amazon Fire HD10 (2017)), your device is a \"${product}\""
+  exit 1
+fi
+
 get_root
+
+tee_version=$((`adb shell getprop ro.boot.tee_version | dos2unix`))
+lk_version=$((`adb shell getprop ro.boot.lk_version | dos2unix`))
+pl_version=$((`adb shell getprop ro.boot.pl_version | dos2unix`))
+
+echo "PL version: ${pl_version} (${max_pl})"
+echo "LK version: ${lk_version} (${max_lk})"
+echo "TZ version: ${tee_version} (${max_tee})"
+echo ""
+
+if [ "$1" = "brick" ] || [ $tee_version -gt $max_tee ] || [ $lk_version -gt $max_lk ] || [ $pl_version -gt $max_pl ] ; then
+  echo "TZ, Preloader or LK are too new, RPMB downgrade necessary (or brick option used)"
+  echo "Brick preloader to continue via bootrom-exploit? (Type \"YES\" to continue)"
+  read YES
+  if [ "$YES" = "YES" ]; then
+    echo "Bricking preloader"
+    adb shell su -c \"echo 0 \> /sys/block/mmcblk0boot0/force_ro\"
+    adb shell su -c \"dd if=/dev/zero of=/dev/block/mmcblk0boot0 bs=512 count=8\"
+    adb shell su -c \"echo -n EMMC_BOOT \> /dev/block/mmcblk0boot0\"
+
+    echo "Flashing LK"
+    adb push bin/lk.bin /data/local/tmp/
+    adb shell su -c \"dd if=/data/local/tmp/lk.bin of=/dev/block/platform/soc/by-name/lk bs=512\" 
+    echo ""
+
+    echo "Flashing TZ"
+    adb push bin/tz.img /data/local/tmp/
+    adb shell su -c \"dd if=/data/local/tmp/tz.img of=/dev/block/platform/soc/by-name/tee1 bs=512\" 
+    adb shell su -c \"dd if=/data/local/tmp/tz.img of=/dev/block/platform/soc/by-name/tee2 bs=512\" 
+    echo ""
+
+    echo "Rebooting..., continue with bootrom-step-minimal.sh"
+    adb shell reboot
+    exit 0
+  fi
+  exit 1
+fi
 
 echo "Your device will be reset to factory defaults..."
 echo "Press Enter to Continue..."
@@ -17,18 +65,22 @@ echo "Dumping GPT"
 adb shell su -c \"dd if=/dev/block/mmcblk0 bs=512 count=34 of=/data/local/tmp/gpt.bin\" 
 adb shell su -c \"chmod 644 /data/local/tmp/gpt.bin\" 
 adb pull /data/local/tmp/gpt.bin gpt/gpt.bin
+echo ""
 
 echo "Modifying GPT"
-modules/gpt.py gpt/gpt.bin
+modules/gpt.py patch gpt/gpt.bin
+echo ""
 
 echo "Flashing temp GPT"
 adb push gpt/gpt.bin.step1.gpt /data/local/tmp/
 adb shell su -c \"dd if=/data/local/tmp/gpt.bin.step1.gpt of=/dev/block/mmcblk0 bs=512 count=34\" 
+echo ""
 
 echo "Preparing for Factory Reset"
 adb shell su -c \"mkdir -p /cache/recovery\"
 adb shell su -c \"echo --wipe_data \> /cache/recovery/command\"
 adb shell su -c \"echo --wipe_cache \>\> /cache/recovery/command\"
+echo ""
 
-echo "Rebooting into recovery"
+echo "Rebooting into Recovery"
 adb reboot recovery
