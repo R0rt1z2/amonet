619f7666622ffc84d3c7b2084c9b6f4d7ee53f7a
Joel Stein <js@funkturm.net>
2019-03-21 19:16:31 +0100

    Wait for userinput because we are factory-resetting

--- fastboot-step.sh
+++ fastboot-step.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "Your device will be reset to factory defaults..."
+echo "Press Enter to Continue..."
+read
+
 fastboot flash recovery_x bin/twrp.img
 fastboot erase userdata
 #fastboot format userdata
