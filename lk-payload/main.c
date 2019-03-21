17861af56e8d2180e66ab7aec6d44012e1268e63
Joel Stein <js@funkturm.net>
2019-03-21 19:16:53 +0100

    remove hex_dump

--- lk-payload/main.c
+++ lk-payload/main.c
@@ -28,38 +28,6 @@ int (*app)() = (void*)0x4BD2CBC1;
 
 uint64_t g_boot, g_boot_x, g_lk, g_misc, g_recovery, g_recovery_x;
 
-void hex_dump(const void* data, size_t size) {
-    char ascii[17];
-    size_t i, j;
-    ascii[16] = '\0';
-    for (i = 0; i < size; ++i) {
-        printf("%02X ", ((unsigned char*)data)[i]);
-        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-            ascii[i % 16] = ((unsigned char*)data)[i];
-        } else {
-            ascii[i % 16] = '.';
-        }
-        if ((i+1) % 8 == 0 || i+1 == size) {
-            printf(" ");
-            if ((i+1) % 16 == 0) {
-                printf("\n");
-                // printf("|  %s \n", ascii);
-            } else if (i+1 == size) {
-                ascii[(i+1) % 16] = '\0';
-                if ((i+1) % 16 <= 8) {
-                    printf(" ");
-                }
-                for (j = (i+1) % 16; j < 16; ++j) {
-                    printf("   ");
-                }
-                // printf("|  %s \n", ascii);
-                printf("\n");
-            }
-        }
-    }
-}
-
-
 int read_func(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) {
     printf("read_func hook\n");
     //hex_dump((void *)0x4BD003DC, 0x100);
