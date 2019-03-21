e39c86759a34810b2dd1270723b1a13826b073a8
Joel Stein <js@funkturm.net>
2019-03-21 19:20:54 +0100

    more checks and allow regenerating gpt from unfinished unlock

--- step-2.sh
+++ step-2.sh
@@ -6,14 +6,10 @@ set -e
 
 . functions.inc
 
-if [ ! -f "gpt/gpt.bin.step2.gpt" ]; then
-  echo "Couldn't find modified GPT, did you run step-1.sh first?"
-  exit 1
-fi
-
 adb wait-for-device
 
 get_root
+
 set +e
 echo "Looking for partition-suffix"
 adb shell su -c \"ls -l /dev/block/platform/soc/by-name\" | grep recovery_tmp
@@ -23,6 +19,9 @@ if [ $? -ne 0 ] ; then
     echo "Didn't find new partitions, did you do step-1.sh first?"
     exit 1
   else
+    echo "Found \"_x\" suffix, it looks like you are rerunning step-2.sh"
+    echo "If this is expected, press enter, otherwise terminate with Ctrl+C"
+    read
     suffix=""
     suffix_b="_x"
   fi
@@ -31,6 +30,30 @@ else
   suffix_b=""
 fi
 set -e
+echo ""
+
+if [ ! -f "gpt/gpt.bin.step2.gpt" ]; then
+  echo "Couldn't find GPT files, regenerating from device"
+  echo ""
+
+  echo "Dumping GPT"
+  [ ! -d gpt-regen ] && mkdir gpt-regen
+  adb shell su -c \"dd if=/dev/block/mmcblk0 bs=512 count=34 of=/data/local/tmp/gpt.bin\" 
+  adb shell su -c \"chmod 644 /data/local/tmp/gpt.bin\" 
+  adb pull /data/local/tmp/gpt.bin gpt-regen/gpt.bin
+  echo ""
+
+  echo "Unpatching GPT"
+  modules/gpt.py unpatch gpt-regen/gpt.bin
+  [ ! -d gpt ] && mkdir gpt
+  cp gpt-regen/gpt.bin.unpatched.gpt gpt/gpt.bin
+  echo ""
+
+  echo "Modifying GPT"
+  modules/gpt.py patch gpt/gpt.bin
+  echo ""
+fi
+
 
 echo "Flashing exploit"
 adb push bin/boot.hdr /data/local/tmp/
@@ -41,28 +64,29 @@ adb shell su -c \"dd if=/data/local/tmp/boot.hdr of=/dev/block/platform/soc/by-n
 adb shell su -c \"dd if=/data/local/tmp/boot.payload of=/dev/block/platform/soc/by-name/recovery${suffix} bs=512 seek=${PAYLOAD_BLOCK}\" 
 echo ""
 
+echo "Flashing LK"
+adb push bin/lk.bin /data/local/tmp/
+adb shell su -c \"dd if=/data/local/tmp/lk.bin of=/dev/block/platform/soc/by-name/lk bs=512\" 
+echo ""
+
+echo "Flashing TZ"
+adb push bin/tz.img /data/local/tmp/
+adb shell su -c \"dd if=/data/local/tmp/tz.img of=/dev/block/platform/soc/by-name/tee1 bs=512\" 
+adb shell su -c \"dd if=/data/local/tmp/tz.img of=/dev/block/platform/soc/by-name/tee2 bs=512\" 
+echo ""
+
 echo "Flashing Preloader"
+adb shell su -c \"echo 0 \> /sys/block/mmcblk0boot0/force_ro\"
 adb push bin/boot0short.img /data/local/tmp/
 adb shell su -c \"dd if=/data/local/tmp/boot0short.img of=/dev/block/mmcblk0boot0 bs=512\" 
 adb push bin/preloader.bin /data/local/tmp/
 adb shell su -c \"dd if=/data/local/tmp/preloader.bin of=/dev/block/mmcblk0boot0 bs=512 seek=520\" 
 echo ""
 
-echo "Flashing TZ"
-adb shell su -c \"echo 0 \> /sys/block/mmcblk0boot0/force_ro\"
-adb push bin/tz.img /data/local/tmp/
-adb shell su -c \"dd if=/data/local/tmp/tz.img of=/dev/block/platform/soc/by-name/tee1 bs=512\" 
-adb shell su -c \"dd if=/data/local/tmp/tz.img of=/dev/block/platform/soc/by-name/tee2 bs=512\" 
-echo ""
-
-echo "Flashing LK"
-adb push bin/lk.bin /data/local/tmp/
-adb shell su -c \"dd if=/data/local/tmp/lk.bin of=/dev/block/platform/soc/by-name/lk bs=512\" 
-echo ""
-
 echo "Flashing final GPT"
 adb push gpt/gpt.bin.step2.gpt /data/local/tmp/
 adb shell su -c \"dd if=/data/local/tmp/gpt.bin.step2.gpt of=/dev/block/mmcblk0 bs=512 count=34\" 
+echo ""
 if [ -f "gpt/gpt.bin.offset" ] ; then
   OFFSET=$(cat gpt/gpt.bin.offset)
   # Check if $OFFSET has some sane value
@@ -70,9 +94,9 @@ if [ -f "gpt/gpt.bin.offset" ] ; then
     echo "Flashing final GPT (backup)"
     adb push gpt/gpt.bin.step2.bak /data/local/tmp/
     adb shell su -c \"dd if=/data/local/tmp/gpt.bin.step2.bak of=/dev/block/mmcblk0 bs=512 seek=${OFFSET}\" 
+    echo ""
   fi
 fi
-echo ""
 
 echo "Flashing TWRP"
 adb push bin/twrp.img /data/local/tmp/
