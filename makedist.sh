63374bffa00e04fd37a88e94561e4e84f38dc742
R0rt1z2 <me@r0rt1z2.com>
2024-06-30 22:32:53 +0200

    Fixes for flashable unlock

--- makedist.sh
+++ makedist.sh
@@ -8,7 +8,7 @@ if [ ! -f bin/preloader.bin ] || [ ! -f bin/tz.img ] || [ ! -f bin/lk.bin ] || [
 fi
 
 mkdir -p dist/unlock/amonet/bin
-cp bin/{preloader.bin,lk.bin,tz.img,twrp.img,boot.hdr,boot.payload} dist/unlock/amonet/bin/
+cp bin/{busybox,preloader.bin,lk.bin,tz.img,twrp.img,boot.hdr,boot.payload} dist/unlock/amonet/bin/
 cp gpt/gpt-ariel-amonet.bin dist/unlock/amonet/bin/
 
 echo -ne "boot-recovery\x00" > dist/unlock/amonet/bin/boot-recovery.bin
@@ -16,7 +16,7 @@ echo -ne "boot-recovery\x00" > dist/unlock/amonet/bin/boot-recovery.bin
 mkdir -p dist/unlock/amonet/modules
 cp modules/{common.py,gpt.py,handshake.py,handshake2.py,logger.py,main.py,mmc.py,functions.py} dist/unlock/amonet/modules/
 
-cp {bootrom-step.sh,fastboot-step.sh,boot-fastboot.sh,boot-recovery.sh} dist/unlock/amonet/
+cp {functions.inc,bootrom-step.sh,fastboot-step.sh,boot-fastboot.sh,boot-recovery.sh} dist/unlock/amonet/
 
 mkdir -p dist/unlock/META-INF/com/google/android
 cp META-INF/com/google/android/{update-binary,updater-script} dist/unlock/META-INF/com/google/android/
