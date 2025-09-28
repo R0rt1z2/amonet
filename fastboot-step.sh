af704ba776abe72f42c71a6abfc0f422a3b6579e
R0rt1z2 <me@r0rt1z2.com>
2025-09-28 13:25:58 +0200

    Update the fastboot script

--- fastboot-step.sh
+++ fastboot-step.sh
@@ -4,9 +4,10 @@ set -e
 
 fastboot flash tee2 bin/tz.img
 fastboot flash recovery bin/twrp.img
-fastboot reboot recovery
+fastboot oem reboot-recovery
 
 echo ""
+echo "The device should now boot into TWRP recovery, indicated by a constantly blinking cyan LED."
 echo ""
-echo "If you don't see the recovery in a few seconds, try pressing the power button twice"
+echo "If you were previously on FireOS 6, consider flashing a FireOS 5-based ROM to avoid a potential boot loop."
 echo ""
